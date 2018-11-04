7674ef077677ba5b18ae8ed3723382e4d81188a6
huangtao00 <397916230@qq.com>
2018-11-04 22:06:20 +0800

    format long

---
+++ ch2/main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    printf("Hello World!\n");
+
+    printf("The minimum value of LONG = %ld\n", LONG_MIN);
+    printf("The maximum value of LONG = %ld\n", LONG_MAX);
+    printf("The maximum value of LONG = %lu\n", ULONG_MAX);
+
+    return 0;
+}
